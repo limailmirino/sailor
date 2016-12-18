88248792551726a2e81d46c2c829edbb46611d77
telefonosuci <telefonosuci@gmail.com>
2016-12-18 23:06:43 +0100

    CORS support and pluralized routes

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -6,42 +6,5 @@
  */
 
 module.exports = {
-  function(req, res) {
-
-    Event.find().exec(function (err, eventList){
-
-      if (err) {
-        return res.negotiate(err);
-      }
-
-      sails.log('Extracted %d events from the DB.', eventList.length);
-      return res.json(eventList);
-
-    });
-
-  },
-  all: function(req, res) {
-    res.send("All the events with " + req.method);
-  },
-
-  error: function(req, res) {
-    res.json(500, {
-      error: 'message'
-    });
-  },
-
-  single: function(req, res) {
-
-    Event.find({'id':''}).exec(function (err, eventList){
-
-      if (err) {
-        return res.negotiate(err);
-      }
-
-      sails.log('Extracted %d events from the DB.', eventList.length);
-      return res.json(eventList);
-
-    });
-
-  }
+  
 };
